13f1753322113892155ee9de98e59ffb2e3c6baa
dam <1782067308@qq.com>
2024-01-19 19:24:22 +0800

    雪花算法

---
+++ dev/increment-backup-serve/src/main/java/org/dam/common/utils/SnowflakeIdUtil.java
@@ -0,0 +1,101 @@
+package org.dam.common.utils;
+
+import org.dam.common.snowflake.IdGeneratorManager;
+import org.dam.common.snowflake.SnowflakeIdInfo;
+import org.dam.common.snowflake.Snowflake;
+
+/**
+ * @Author dam
+ * @create 2024/1/19 17:34
+ */
+public final class SnowflakeIdUtil {
+
+    /**
+     * 雪花算法对象
+     */
+    private static Snowflake SNOWFLAKE;
+
+    /**
+     * 初始化雪花算法
+     */
+    public static void initSnowflake(Snowflake snowflake) {
+        SnowflakeIdUtil.SNOWFLAKE = snowflake;
+    }
+
+    /**
+     * 获取雪花算法实例
+     */
+    public static Snowflake getInstance() {
+        return SNOWFLAKE;
+    }
+
+    /**
+     * 获取雪花算法下一个 ID
+     */
+    public static long nextId() {
+        return SNOWFLAKE.nextId();
+    }
+
+    /**
+     * 获取雪花算法下一个字符串类型 ID
+     */
+    public static String nextIdStr() {
+        return Long.toString(nextId());
+    }
+
+    /**
+     * 解析雪花算法生成的 ID 为对象
+     */
+    public static SnowflakeIdInfo parseSnowflakeId(String snowflakeId) {
+        return SNOWFLAKE.parseSnowflakeId(Long.parseLong(snowflakeId));
+    }
+
+    /**
+     * 解析雪花算法生成的 ID 为对象
+     */
+    public static SnowflakeIdInfo parseSnowflakeId(long snowflakeId) {
+        return SNOWFLAKE.parseSnowflakeId(snowflakeId);
+    }
+
+    /**
+     * 根据 {@param serviceId} 生成雪花算法 ID
+     */
+    public static long nextIdByService(String serviceId) {
+        return IdGeneratorManager.getDefaultServiceIdGenerator().nextId(Long.parseLong(serviceId));
+    }
+
+    /**
+     * 根据 {@param serviceId} 生成字符串类型雪花算法 ID
+     */
+    public static String nextIdStrByService(String serviceId) {
+        return IdGeneratorManager.getDefaultServiceIdGenerator().nextIdStr(Long.parseLong(serviceId));
+    }
+
+    /**
+     * 根据 {@param serviceId} 生成字符串类型雪花算法 ID
+     */
+    public static String nextIdStrByService(String resource, long serviceId) {
+        return IdGeneratorManager.getIdGenerator(resource).nextIdStr(serviceId);
+    }
+
+    /**
+     * 根据 {@param serviceId} 生成字符串类型雪花算法 ID
+     */
+    public static String nextIdStrByService(String resource, String serviceId) {
+        return IdGeneratorManager.getIdGenerator(resource).nextIdStr(serviceId);
+    }
+
+    /**
+     * 解析雪花算法生成的 ID 为对象
+     */
+    public static SnowflakeIdInfo parseSnowflakeServiceId(String snowflakeId) {
+        return IdGeneratorManager.getDefaultServiceIdGenerator().parseSnowflakeId(Long.parseLong(snowflakeId));
+    }
+
+    /**
+     * 解析雪花算法生成的 ID 为对象
+     */
+    public static SnowflakeIdInfo parseSnowflakeServiceId(String resource, String snowflakeId) {
+        return IdGeneratorManager.getIdGenerator(resource).parseSnowflakeId(Long.parseLong(snowflakeId));
+    }
+}
---
+++ dev/increment-backup-serve/src/main/java/org/dam/controller/SourceController.java
@@ -0,0 +1,64 @@
+package org.dam.controller;
+
+import org.dam.common.page.PageRequest;
+import org.dam.common.page.PageResponse;
+import org.dam.common.result.Result;
+import org.dam.common.result.Results;
+import org.dam.entity.BackupSource;
+import org.dam.entity.User;
+import org.dam.mapper.UserMapper;
+import org.dam.service.BackupSourceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+/**
+ * @Author dam
+ * @create 2024/1/18 20:37
+ */
+@RestController
+@RequestMapping("/source")
+public class SourceController {
+
+    @Autowired
+    private BackupSourceService sourceService;
+
+    /**
+     * 增添数据
+     */
+    @GetMapping("/insertSource")
+    public Result insertSource(String rootPath, String backupName) {
+        BackupSource backupSource = new BackupSource();
+        backupSource.setRootPath(rootPath);
+        backupSource.setBackupName(backupName);
+        sourceService.save(backupSource);
+        return Results.success();
+    }
+
+    /**
+     * 查询数据
+     */
+    @PostMapping("/list")
+    public Result<PageResponse<BackupSource>> list(PageRequest pageRequest) {
+        return Results.success(sourceService.pageBackupSource(pageRequest));
+    }
+
+    /**
+     * 删除数据
+     */
+    @DeleteMapping("/removeById")
+    public Result removeById(Integer id) {
+        sourceService.removeById(id);
+        return Results.success();
+    }
+
+    /**
+     * 修改数据
+     */
+    @PostMapping("/update")
+    public Result update(BackupSource source) {
+        sourceService.updateById(source);
+        return Results.success();
+    }
+}
