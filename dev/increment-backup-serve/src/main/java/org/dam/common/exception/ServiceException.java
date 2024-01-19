c721a8d4754d6ec682881ac4aad2bcdaa6d6d5f0
dam <1782067308@qq.com>
2024-01-19 14:16:59 +0800

    全局异常处理

---
+++ dev/increment-backup-serve/src/main/java/org/dam/common/exception/ServiceException.java
@@ -0,0 +1,35 @@
+package org.dam.common.exception;
+
+import org.dam.common.errorcode.BaseErrorCode;
+import org.dam.common.errorcode.IErrorCode;
+
+/**
+ * @Author dam
+ * @create 2024/1/19 14:10
+ */
+public class ServiceException extends AbstractException {
+
+    public ServiceException(String message) {
+        this(message, null, BaseErrorCode.SERVICE_ERROR);
+    }
+
+    public ServiceException(IErrorCode errorCode) {
+        this(null, errorCode);
+    }
+
+    public ServiceException(String message, IErrorCode errorCode) {
+        this(message, null, errorCode);
+    }
+
+    public ServiceException(String message, Throwable throwable, IErrorCode errorCode) {
+        super(message, throwable, errorCode);
+    }
+
+    @Override
+    public String toString() {
+        return "ServiceException{" +
+                "code='" + errorCode + "'," +
+                "message='" + errorMessage + "'" +
+                '}';
+    }
+}
