dc314708085e04e9d5a10980c8689e97b55ef4ca
dam <1782067308@qq.com>
2024-01-19 11:35:45 +0800

    整合SQLite增删改查测试，用户表目前对整个系统没作用，只是用来测试整合MybatisPlus和SQLite是否可以正常使用

---
+++ dev/increment-backup-serve/src/main/java/org/dam/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package org.dam.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.dam.entity.User;
+
+/**
+* @author mac
+* @description 针对表【user】的数据库操作Mapper
+* @createDate 2024-01-18 21:12:12
+* @Entity generator.entity.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
