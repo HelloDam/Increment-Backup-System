8b99a3fd149602b39bded3f5a3c6503ed422f22b
dam <1782067308@qq.com>
2024-01-19 19:26:32 +0800

    客户异常

---
+++ dev/increment-backup-serve/src/main/java/org/dam/common/exception/ClientException.java
@@ -0,0 +1,35 @@
+package org.dam.common.exception;
+
+import org.dam.common.errorcode.BaseErrorCode;
+import org.dam.common.errorcode.IErrorCode;
+
+/**
+ * @Author dam
+ * @create 2024/1/19 14:28
+ */
+public class ClientException extends AbstractException {
+
+    public ClientException(IErrorCode errorCode) {
+        this(null, null, errorCode);
+    }
+
+    public ClientException(String message) {
+        this(message, null, BaseErrorCode.CLIENT_ERROR);
+    }
+
+    public ClientException(String message, IErrorCode errorCode) {
+        this(message, null, errorCode);
+    }
+
+    public ClientException(String message, Throwable throwable, IErrorCode errorCode) {
+        super(message, throwable, errorCode);
+    }
+
+    @Override
+    public String toString() {
+        return "ClientException{" +
+                "code='" + errorCode + "'," +
+                "message='" + errorMessage + "'" +
+                '}';
+    }
+}
