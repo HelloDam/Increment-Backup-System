244d4ce1691a08abf14ab38bd9ec83d26929a7c8
dam <1782067308@qq.com>
2024-01-20 17:10:14 +0800

    统一跨域处理

---
+++ dev/increment-backup-serve/src/main/java/org/dam/config/CorsConfiguration.java
@@ -0,0 +1,27 @@
+package org.dam.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Author dam
+ * @create 2024/1/20 17:06
+ */
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")//允许哪些域访问
+                .allowedMethods("GET","POST","PUT","DELETE","HEAD","OPTIONS")//允许哪些方法访问
+                .allowCredentials(true)//是否允许携带cookie
+                .maxAge(3600)//设置浏览器询问的有效期
+                .allowedHeaders("*");//
+    }
+}
