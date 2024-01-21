3c45b2b8528b2aedbdb595c87ab86a5337c2892a
dam <1782067308@qq.com>
2024-01-21 10:45:01 +0800

    使用element plus的图标

--- dev/increment-backup-client/src/main.js
+++ dev/increment-backup-client/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
 
@@ -7,6 +8,9 @@ import routes from './router/index.js'
 import * as VueRouter from 'vue-router';
 
 const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
 
 const router = VueRouter.createRouter({
     // 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
