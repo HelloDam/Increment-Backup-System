fe433043da2d5e9afc91a8658bab6a2e4192f902
dam <1782067308@qq.com>
2024-01-24 10:12:52 +0800

    目标备份目录请求接口

---
+++ dev/increment-backup-client/src/api/backupTargetApi.js
@@ -0,0 +1,70 @@
+import request from '../utils/axiosRequest.js'
+
+const apiName = '/target'
+
+export default {
+
+    /**
+     * 条件分页查询
+     * @param data
+     * @returns {*}
+     */
+    list(data) {
+        return request({
+            url: `${apiName}/list`,
+            method: "post",
+            data: data
+        })
+    },
+
+    /**
+     * 根据ID数组批量删除
+     * @param ids
+     * @returns {*}
+     */
+    removeByIds(ids) {
+        return request({
+            url: `${apiName}/removeByIds`,
+            method: "post",
+            data: ids
+        })
+    },
+
+    /**
+     * 根据ID从数据库中获取数据
+     * @param id
+     * @returns {*}
+     */
+    getById(id) {
+        return request({
+            url: `${apiName}/getById/${id}`,
+            method: "get"
+        })
+    },
+
+    /**
+     * 增加数据
+     * @param data
+     * @returns {*}
+     */
+    save(data) {
+        return request({
+            url: `${apiName}/save`,
+            method: "post",
+            data: data
+        })
+    },
+
+    /**
+     * 修改数据
+     * @param data
+     * @returns {*}
+     */
+    update(data) {
+        return request({
+            url: `${apiName}/update`,
+            method: "post",
+            data: data
+        })
+    }
+}
