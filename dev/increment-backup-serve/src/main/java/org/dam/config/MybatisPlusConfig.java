b134eb289d59a0ce97de2ae0b2560dd351f60c02
dam <1782067308@qq.com>
2024-01-25 20:30:23 +0800

    补充分页配置，修正分页失败的bug

--- dev/increment-backup-serve/src/main/java/org/dam/config/MybatisPlusConfig.java
+++ dev/increment-backup-serve/src/main/java/org/dam/config/MybatisPlusConfig.java
@@ -1,8 +1,12 @@
 package org.dam.config;
 
+import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -29,5 +33,18 @@ public class MybatisPlusConfig implements MetaObjectHandler {
     @Override
     public void updateFill(MetaObject metaObject) {
         strictInsertFill(metaObject, "updateTime", Date::new, Date.class);
+    }
+
+    /**
+     * 配置分页插件，获取total那些才不会出错
+     *
+     * @return
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        //  如果有多数据源可以不配具体类型 否则都建议配上具体的DbType
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.SQLITE));
+        return interceptor;
     }
 }
