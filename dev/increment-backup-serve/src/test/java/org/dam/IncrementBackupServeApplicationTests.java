90e073158e55c79a421ee200dbe0f3e7fed76cf7
dam <1782067308@qq.com>
2024-01-25 10:21:43 +0800

    service测试类

--- dev/increment-backup-serve/src/test/java/org/dam/IncrementBackupServeApplicationTests.java
+++ dev/increment-backup-serve/src/test/java/org/dam/IncrementBackupServeApplicationTests.java
@@ -1,13 +1,41 @@
 package org.dam;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import org.dam.entity.BackupTask;
+import org.dam.service.BackupTaskService;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+import java.util.List;
+
+//@SpringBootTest
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {IncrementBackupServeApplication.class})
+@SpringBootTest( webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class IncrementBackupServeApplicationTests {
 
+    @Autowired
+    private BackupTaskService backupTaskService;
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void dasd(){
+        BackupTask backupTask = new BackupTask("dasdas", "dsadas",
+                10, 0, 10L, 0L, 0,"0.0");
+        backupTaskService.save(backupTask);
+    }
+
+    @Test
+    void dashidas(){
+        QueryWrapper<BackupTask> backupTaskQueryWrapper = new QueryWrapper<>();
+            backupTaskQueryWrapper.ne("backup_status", 2);
+        List<BackupTask> taskList = backupTaskService.list(backupTaskQueryWrapper);
     }
 
 }
