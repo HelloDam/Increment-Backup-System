d2f58a1a3f520d928eac6cd24c408f130b5ff625
dam <1782067308@qq.com>
2024-01-25 10:23:23 +0800

    引入正在备份的任务表格

--- dev/increment-backup-client/src/views/index.vue
+++ dev/increment-backup-client/src/views/index.vue
@@ -1,61 +1,104 @@
 <template>
-  <div class="container">
+    <div class="container">
 
-    <el-menu
-        :default-active="activeIndex"
-        class="el-menu-demo"
-        mode="horizontal"
-        :ellipsis="false"
-        @select="handleSelect"
-    >
-      <el-menu-item index="0">
-        <img
-            style="width: 100px"
-            src="../assets/element-plus-logo.svg"
-            alt="Element logo"
-        />
-      </el-menu-item>
-      <div class="flex-grow"/>
-      <el-menu-item index="1">
-        <el-badge :value="12" class="item">
-          备份任务
-        </el-badge>
-      </el-menu-item>
-      <el-menu-item index="2">已备份文件</el-menu-item>
-      <el-menu-item index="3">文件备份记录</el-menu-item>
-      <!--      <el-sub-menu index="2">-->
-      <!--        <template #title>Workspace</template>-->
-      <!--        <el-menu-item index="2-1">item one</el-menu-item>-->
-      <!--        <el-menu-item index="2-2">item two</el-menu-item>-->
-      <!--        <el-menu-item index="2-3">item three</el-menu-item>-->
-      <!--        <el-sub-menu index="2-4">-->
-      <!--          <template #title>item four</template>-->
-      <!--          <el-menu-item index="2-4-1">item one</el-menu-item>-->
-      <!--          <el-menu-item index="2-4-2">item two</el-menu-item>-->
-      <!--          <el-menu-item index="2-4-3">item three</el-menu-item>-->
-      <!--        </el-sub-menu>-->
-      <!--      </el-sub-menu>-->
-    </el-menu>
+        <el-menu
+                :default-active="activeIndex"
+                class="menuClass"
+                mode="horizontal"
+                :ellipsis="false"
+                @select="handleSelectActive"
+        >
+            <el-menu-item index="0">
+                <img
+                        style="width: 100px"
+                        src="../assets/element-plus-logo.svg"
+                        alt="Element logo"
+                />
+            </el-menu-item>
+            <div class="flex-grow"/>
+            <el-menu-item index="1">
+                <div>
+                    备份任务
+                    <span class="badge">{{ backupTaskList.length }}</span>
+                </div>
+            </el-menu-item>
+            <el-menu-item index="2">已备份文件</el-menu-item>
+            <el-menu-item index="3">文件备份记录</el-menu-item>
+        </el-menu>
 
-    <!--    <div class="head">-->
-    <!--      <div></div>-->
-    <!--      <div style=" font-size: 2rem;font-weight: 500;">-->
-    <!--        文 件 增 量 备 份 系 统-->
-    <!--      </div>-->
-    <!--      <div></div>-->
-    <!--    </div>-->
-    <div class="bodyDiv">
+        <!--    <div class="head">-->
+        <!--      <div></div>-->
+        <!--      <div style=" font-size: 2rem;font-weight: 500;">-->
+        <!--        文 件 增 量 备 份 系 统-->
+        <!--      </div>-->
+        <!--      <div></div>-->
+        <!--    </div>-->
+        <div class="bodyDiv">
 
-      <div class="tableDiv">
-        <BackupSourceTable @changeSourceChange="changeSourceChange()" ref="backupSourceTable"></BackupSourceTable>
-      </div>
-      <div style="width: 10px"></div>
-      <div class="tableDiv">
-        <BackupTargetTable :selectBackupSource="selectBackupSource"></BackupTargetTable>
-      </div>
+            <div class="tableDiv">
+                <BackupSourceTable @changeSourceChange="changeSourceChange()"
+                                   ref="backupSourceTable"></BackupSourceTable>
+            </div>
+            <div style="width: 10px"></div>
+            <div class="tableDiv">
+                <BackupTargetTable :selectBackupSource="selectBackupSource"></BackupTargetTable>
+            </div>
+        </div>
+
+        <el-dialog
+                v-model="backupTaskListDialogVisible"
+                title="任务列表"
+                width="85%"
+        >
+            <el-table :data="backupTaskList">
+                <el-table-column type="selection" width="55"/>
+                <el-table-column prop="id" label="编号" width="100" :show-overflow-tooltip="true"/>
+                <el-table-column prop="backupSourceRoot" label="备份数据源根目录" width="200"
+                                 :show-overflow-tooltip="true"/>
+                <el-table-column prop="backupTargetRoot" label="备份目标根目录" width="200"
+                                 :show-overflow-tooltip="true"/>
+                <el-table-column prop="backupProgress" label="备份进度" width="200" :show-overflow-tooltip="true">
+                    <template #default="scope">
+                        <!-- 进度条 -->
+                        <el-progress
+                                :percentage="scope.row.backupProgress"
+                                :stroke-width="15"
+                                :status="scope.row.backupStatus==2?'success':null"
+                                striped
+                                :striped-flow="scope.row.backupStatus==1?true:false"
+                                :duration="10"
+                        />
+                    </template>
+                </el-table-column>
+                <el-table-column prop="backupStatus" label="状态" width="100" :show-overflow-tooltip="true">
+                    <template #default="scope">
+                        <el-tag class="ml-2" v-if="scope.row.backupStatus==0">刚创建</el-tag>
+                        <el-tag class="ml-2" type="warning" v-if="scope.row.backupStatus==1">进行中</el-tag>
+                        <el-tag class="ml-2" type="success" v-if="scope.row.backupStatus==2">完 成</el-tag>
+                        <el-tag class="ml-2" type="danger" v-if="scope.row.backupStatus==3">失 败</el-tag>
+                    </template>
+                </el-table-column>
+                <el-table-column prop="totalFileNum" label="总文件数" width="120"
+                                 :show-overflow-tooltip="true"/>
+                <el-table-column prop="finishFileNum" label="已备份文件数" width="150"
+                                 :show-overflow-tooltip="true"/>
+                <el-table-column prop="totalByteNum" label="总字节数" width="150"
+                                 :show-overflow-tooltip="true"/>
+                <el-table-column prop="finishByteNum" label="已备份字节数" width="150"
+                                 :show-overflow-tooltip="true"/>
+                <el-table-column prop="createTime" label="创建时间" width="180" :show-overflow-tooltip="true"
+                                 :formatter="formatDate"/>
+                <el-table-column prop="updateTime" label="修改时间" width="180" :show-overflow-tooltip="true"
+                                 :formatter="formatDate"/>
+            </el-table>
+            <template #footer>
+          <span class="dialog-footer">
+            <el-button @click="backupTaskListDialogVisible = false">关 闭</el-button>
+          </span>
+            </template>
+        </el-dialog>
+
     </div>
-
-  </div>
 </template>
 
 <script>
@@ -64,63 +107,127 @@ import BackupSourceTable from "../components/BackupSourceTable.vue";
 import BackupTargetTable from "../components/BackupTargetTable.vue";
 
 export default {
-  components: {BackupSourceTable, BackupTargetTable},
-  data() {
-    return {
-      // 数据源
-      selectBackupSource: {},
-    };
-  },
-  computed: {},
-  watch: {},
-  methods: {
-    changeSourceChange() {
-      console.log("子组件选中数据源更新");
-      this.selectBackupSource = this.$refs.backupSourceTable.selectBackupSource;
+    components: {BackupSourceTable, BackupTargetTable},
+    data() {
+        return {
+            // 数据源
+            selectBackupSource: {},
+            // 备份任务列表
+            backupTaskList: [],
+            activeIndex: 0,
+            backupTaskListDialogVisible: false,
+        };
     },
-  },
-  beforeCreate() {
-  }
-  ,
+    computed: {},
+    watch: {},
+    methods: {
+        changeSourceChange() {
+            console.log("子组件选中数据源更新");
+            this.selectBackupSource = this.$refs.backupSourceTable.selectBackupSource;
+        },
+        /**
+         * 初始化websocket连接
+         */
+        initWebSocket() {
+            let socket = {};
+            let _this = this;
+            if (typeof (WebSocket) == "undefined") {
+                console.log("您的浏览器不支持WebSocket协议");
+            } else {
+                console.log("您的浏览器支持WebSocket协议");
+                let socketUrl = "ws://localhost:8899/websocket/Admin";
+                // 开启一个websocket服务
+                socket = new WebSocket(socketUrl);
+                //打开日志事件
+                socket.onopen = function () {
+                    console.log("websocket已打开");
+                };
+                //浏览器端收消息，获得从服务端发送过来的文本消息
+                socket.onmessage = function (msg) {
+                    // 对收到的json数据进行解析
+                    // let data = JSON.parse(JSON.stringify(msg));
+                    // console.log("收到备份进度消息：" + JSON.stringify(msg));
+                    // console.log(msg);
+                    _this.backupTaskList = JSON.parse(msg.data);
+                    console.log("收到备份任务进度消息：");
 
-  created() {
+                };
+                //关闭事件
+                socket.onclose = function () {
+                    console.log("websocket已关闭");
+                };
+                //发生了错误事件
+                socket.onerror = function () {
+                    console.log("websocket发生错误");
+                }
+            }
+        },
+        /**
+         /**
+         /**
+         /**
+         * 切换所激活主题
+         */
+        handleSelectActive(activeIndex) {
+            console.log("handleSelectActive:" + JSON.stringify(activeIndex));
+            if (activeIndex === '1') {
+                console.log("查看任务列表");
+                for (let i = 0; i < this.backupTaskList.length; i++) {
+                    console.log(JSON.stringify(this.backupTaskList[i]));
+                }
+                this.backupTaskListDialogVisible = true;
+            }
+        },
+        /**
+         * 格式化日期和时间
+         */
+        formatDate(row, column, cellValue, index) {
+            let date = new Date(cellValue);
+            return date.toLocaleDateString() + " " + date.toLocaleTimeString();
+        },
+    },
+    beforeCreate() {
+    }
+    ,
 
-  }
-  ,
+    created() {
+        this.initWebSocket();
+    }
+    ,
 
-  beforeMount() {
+    beforeMount() {
 
-  }
-  ,
+    }
+    ,
 
-  mounted() {
+    mounted() {
 
-  }
-  ,
+    }
+    ,
 
-  beforeUpdate() {
+    beforeUpdate() {
 
-  }
-  ,
+    }
+    ,
 
-  updated() {
+    updated() {
 
-  }
-  ,
+    }
+    ,
 
-  beforeDestroy() {
+    beforeDestroy() {
 
-  }
-  ,
-  destroyed() {
+    }
+    ,
+    destroyed() {
 
-  }
-  ,
+    }
+    ,
 
-  activated() {
+    activated() {
 
-  }
-  ,
+    }
+    ,
 }
 
 
@@ -143,9 +250,29 @@ export default {
   padding: 1rem;
   font-family: Arial, Helvetica, sans-serif, Times New Roman;
 
-  .flex-grow {
-    flex-grow: 1;
+  .menuClass {
+    border-radius: 5px;
+
+    .badge {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background-color: #f00;
+      color: #fff;
+      font-size: 12px;
+      height: 15px;
+      width: 15px;
+      border-radius: 10%;
+      position: absolute;
+      top: 5px;
+      right: 5px;
+    }
+
+    .flex-grow {
+      flex-grow: 1;
+    }
   }
+
 
   .item {
     margin-top: 0px;
