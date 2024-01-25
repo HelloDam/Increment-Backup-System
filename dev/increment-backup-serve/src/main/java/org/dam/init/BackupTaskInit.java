4f297ed5547a9ef16fa7fdb17702dd7090b0090d
dam <1782067308@qq.com>
2024-01-25 22:22:10 +0800

    增加空行

--- dev/increment-backup-serve/src/main/java/org/dam/init/BackupTaskInit.java
+++ dev/increment-backup-serve/src/main/java/org/dam/init/BackupTaskInit.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Component;
 @Component
 @Slf4j
 public class BackupTaskInit implements CommandLineRunner {
+
     @Autowired
     private BackupTaskService backupTaskService;
     @Override
