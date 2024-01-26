783f0823083ffb778321f7190a1bbd5f7318f1a9
dam <1782067308@qq.com>
2024-01-26 15:30:38 +0800

    footer

---
+++ dev/increment-backup-client/src/components/AppFooter.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="by-app-footer">
+    Copyright&nbsp;&nbsp;&copy;2023&nbsp;&nbsp;HelloDam&nbsp;&nbsp;All&nbsp;Rights&nbsp;Reserved
+  </div>
+</template>
+
+<script>
+
+export default {
+  computed: {},
+  data() {
+    return {}
+  },
+  watch: {},
+  methods: {}
+}
+</script>
+
+<style scoped lang="scss">
+
+.by-app-footer {
+  height: 20px;
+  width: 100%;
+  text-align: center;
+  font-size: 12px;
+  line-height: 20px;
+  color: #808080;
+  letter-spacing: 1px;
+  background: white;
+}
+</style>
