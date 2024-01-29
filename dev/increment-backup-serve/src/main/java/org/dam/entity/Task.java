a47ce83207134c857aeeb96a41de4d8b19306f9c
dam <1782067308@qq.com>
2024-01-29 17:57:31 +0800

    增加一个属性

--- dev/increment-backup-serve/src/main/java/org/dam/entity/Task.java
+++ dev/increment-backup-serve/src/main/java/org/dam/entity/Task.java
@@ -3,6 +3,8 @@ package org.dam.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.List;
+
 /**
  * @Author dam
  * @create 2024/1/19 14:26
@@ -12,5 +14,5 @@ import lombok.Data;
 public class Task {
     private BackupSource source;
     private BackupTarget target;
-
+    private List<BackupTarget> targetList;
 }
