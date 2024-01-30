46a136437a995d1d0454e34c3cf26b6fdba63095
dam <1782067308@qq.com>
2024-01-30 22:24:46 +0800

    系统常量类

---
+++ dev/increment-backup-serve/src/main/java/org/dam/common/constant/SystemConstant.java
@@ -0,0 +1,9 @@
+package org.dam.common.constant;
+
+/**
+ * @Author dam
+ * @create 2024/1/30 18:47
+ */
+public class SystemConstant {
+    public static final String ZIP_SUFFIX = ".easyBackupZip";
+}
