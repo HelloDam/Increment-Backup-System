3d2ab069ae5102e66726a2cc17d4f073319c9ad1
dam <1782067308@qq.com>
2024-02-21 17:06:10 +0800

    optimize：按照大小来显示占用存储空间

---
+++ dev/increment-backup-client/src/utils/storageUtil.js
@@ -0,0 +1,11 @@
+export default {
+    formatBytes(bytes) {
+        const units = ['byte', 'KB', 'MB', 'GB', 'TB'];
+        let i = 0;
+        while (bytes >= 1024 && i < units.length - 1) {
+            bytes /= 1024;
+            i++;
+        }
+        return bytes.toFixed(2) + ' ' + units[i];
+    }
+}
